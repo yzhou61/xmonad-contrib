7262730c7e4666870554cb533ee23e92c6929545
Yu Zhou <yzhou61@gmail.com>
2015-05-17 12:59:10 -0700

    Add multiple xft fonts support

--- XMonad/Util/Font.hs
+++ XMonad/Util/Font.hs
@@ -42,13 +42,14 @@ import Data.Maybe
 import Data.List
 import Graphics.X11.Xft
 import Graphics.X11.Xrender
+import Data.Char (ord)
 #endif
 
 -- Hide the Core Font/Xft switching here
 data XMonadFont = Core FontStruct
                 | Utf8 FontSet
 #ifdef XFT
-                | Xft  XftFont
+                | Xft  [XftFont]
 #endif
 
 -- $usage
@@ -98,31 +99,54 @@ initXMF s =
 #ifdef XFT
   if xftPrefix `isPrefixOf` s then
      do dpy <- asks display
-        xftdraw <- io $ xftFontOpen dpy (defaultScreenOfDisplay dpy) (drop (length xftPrefix) s)
+        xftdraw <- mapM (openFont dpy) (wordsWhen (==',') (drop (length xftPrefix) s))
         return (Xft xftdraw)
   else
 #endif
       fmap Utf8 $ initUtf8Font s
 #ifdef XFT
   where xftPrefix = "xft:"
+        openFont dpy fontName = do
+          xftdraw <- io $ xftFontOpen dpy (defaultScreenOfDisplay dpy) fontName
+          return xftdraw
+        wordsWhen p string =  case dropWhile p string of
+                      "" -> []
+                      string' -> w : wordsWhen p string''
+                            where (w, string'') = break p string'
 #endif
 
 releaseXMF :: XMonadFont -> X ()
 #ifdef XFT
-releaseXMF (Xft xftfont) = do
+releaseXMF (Xft xftfonts) = do
   dpy <- asks display
-  io $ xftFontClose dpy xftfont
+  mapM_ (closeFont dpy) xftfonts
+  where closeFont dpy font = do
+          io $ xftFontClose dpy font
 #endif
 releaseXMF (Utf8 fs) = releaseUtf8Font fs
 releaseXMF (Core fs) = releaseCoreFont fs
 
+#ifdef XFT
+xftTxtExtents' :: Display -> [XftFont] -> String -> IO XGlyphInfo
+xftTxtExtents' d fs string = do
+    chunks <- getChunks d fs string
+    let (_, _, gi, _, _) = last chunks
+    return gi
+
+xftfont_ascent' :: [XftFont] -> IO Int
+xftfont_ascent' = (fmap maximum) . (mapM xftfont_ascent)
+
+xftfont_descent' :: [XftFont] -> IO Int
+xftfont_descent' = (fmap maximum) . (mapM xftfont_descent)
+
+#endif
 
 textWidthXMF :: MonadIO m => Display -> XMonadFont -> String -> m Int
 textWidthXMF _   (Utf8 fs) s = return $ fi $ wcTextEscapement fs s
 textWidthXMF _   (Core fs) s = return $ fi $ textWidth fs s
 #ifdef XFT
 textWidthXMF dpy (Xft xftdraw) s = liftIO $ do
-    gi <- xftTextExtents dpy xftdraw s
+    gi <- xftTxtExtents' dpy xftdraw s
     return $ xglyphinfo_xOff gi
 #endif
 
@@ -137,8 +161,8 @@ textExtentsXMF (Core fs) s = do
   return (a,d)
 #ifdef XFT
 textExtentsXMF (Xft xftfont) _ = io $ do
-  ascent  <- fi `fmap` xftfont_ascent  xftfont
-  descent <- fi `fmap` xftfont_descent xftfont
+  ascent  <- fi `fmap` xftfont_ascent' xftfont
+  descent <- fi `fmap` xftfont_descent' xftfont
   return (ascent, descent)
 #endif
 
@@ -180,7 +204,7 @@ printStringXMF dpy drw fs@(Xft font) gc fc bc x y s = do
       visual   = defaultVisualOfScreen screen
   bcolor <- stringToPixel dpy bc
   (a,d)  <- textExtentsXMF fs s
-  gi <- io $ xftTextExtents dpy font s
+  gi <- io $ xftTxtExtents' dpy font s
   io $ setForeground dpy gc bcolor
   io $ fillRectangle dpy drw gc (x - fi (xglyphinfo_x gi))
                                 (y - fi a)
@@ -188,9 +212,67 @@ printStringXMF dpy drw fs@(Xft font) gc fc bc x y s = do
                                 (fi $ a + d)
   io $ withXftDraw dpy drw visual colormap $
          \draw -> withXftColorName dpy visual colormap fc $
-                   \color -> xftDrawString draw color font x y s
+                   \color -> xftDrawString' draw color font (toInteger x) (toInteger y) s
 #endif
 
 -- | Short-hand for 'fromIntegral'
 fi :: (Integral a, Num b) => a -> b
 fi = fromIntegral
+
+#ifdef XFT
+xftDrawString' :: XftDraw ->
+                  XftColor ->
+                  [XftFont] ->
+                  Integer ->
+                  Integer ->
+                  String -> IO ()
+xftDrawString' d c fs x y string = do
+    dpy <- xftDrawDisplay d
+    chunks <- getChunks dpy fs string
+    mapM_ (\(f, s, _, xo, yo) -> xftDrawString d c f (x+xo) (y+yo) s) chunks
+
+xftCharExists' :: Display -> XftFont -> Char -> IO Bool
+xftCharExists' d f c = bool `fmap` xftCharExists d f (fi $ ord c)
+  where
+    bool 0 = False
+    bool _ = True
+ 
+-- Split string and determine fonts/offsets for individual parts
+getChunks :: Display -> [XftFont] -> [Char] ->
+             IO [(XftFont, String, XGlyphInfo, Integer, Integer)]
+getChunks disp fts str = do
+    chunks <- getFonts disp fts str
+    getOffsets (XGlyphInfo 0 0 0 0 0 0) chunks
+  where
+    -- Split string and determine fonts for individual parts
+    getFonts _ [] _ = return []
+    getFonts _ _ [] = return []
+    getFonts _ [ft] s = return [(ft, s)]
+    getFonts d fonts@(ft:_) s = do
+        -- Determine which glyph can be rendered by current font
+        glyphs <- mapM (xftCharExists' d ft) s
+        -- Split string into parts that can/cannot be rendered
+        let splits = split (runs glyphs) s
+        -- Determine which font to render each chunk with
+        concat `fmap` mapM (getFont d fonts) splits
+
+    -- Determine fonts for substrings
+    getFont _ [] _ = return []
+    getFont _ [ft] (_, s) = return [(ft, s)] -- Last font, use it
+    getFont _ (ft:_) (True, s) = return [(ft, s)] -- Current font can render this substring
+    getFont d (_:fs) (False, s) = getFonts d fs s -- Fallback to next font
+
+    -- Helpers
+    runs [] = []
+    runs (x:xs) = let (h, t) = span (==x) xs in (x, length h + 1) : runs t
+    split [] _ = []
+    split ((x, c):xs) s = let (h, t) = splitAt c s in (x, h) : split xs t
+
+    -- Determine coordinates for chunks using extents
+    getOffsets _ [] = return []
+    getOffsets (XGlyphInfo _ _ x y xo yo) ((f, s):chunks) = do
+        (XGlyphInfo w' h' _ _ xo' yo') <- xftTextExtents disp f s
+        let gi = XGlyphInfo (xo+w') (yo+h') x y (xo+xo') (yo+yo')
+        rest <- getOffsets gi chunks
+        return $ (f, s, gi, fromIntegral xo, fromIntegral yo) : rest
+#endif
